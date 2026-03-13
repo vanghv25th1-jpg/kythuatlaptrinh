ac79259b7a3604fb80dd82db25edd218ffac3754
PMT201 <PMT201@PMT3018>
2026-03-13 11:09:23 +0700

    vvvav

---
+++ kythuatlaptrinh/buoi1/buoi11.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+struct person {
+	int id;
+	string name;
+	int age;
+	string address;
+
+};
+int main() {
+	vector<person>list;
+	do {
+		sytem("cls");
+		cout <<
+	}
+
+}
